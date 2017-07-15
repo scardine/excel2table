41804b92f79c57d49e345412fc1957def220c0bb
chfw <wangc_2011@hotmail.com>
2017-07-15 20:46:55 +0100

    more test coverage

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -13,7 +13,26 @@ def test_cli():
     os.unlink('goog.html')
 
 
+@patch('click.getchar')
+def test_cli_prompt(fake_getchar):
+    fake_getchar.return_value = 'y'
+    runner = CliRunner()
+
+    result = runner.invoke(cli, ['sample/goog.ods', 'goog.html'])
+    result = runner.invoke(cli, ['sample/goog.ods', 'goog.html'])
+    eq_(result.exit_code, 0)
+    os.unlink('goog.html')
+
+
 def test_vs():
+    runner = CliRunner()
+
+    result = runner.invoke(cli, ['-vs', '100', 'sample/goog.ods', 'goog.html'])
+    eq_(result.exit_code, 0)
+    os.unlink('goog.html')
+
+
+def test_vs_disabled():
     runner = CliRunner()
 
     result = runner.invoke(cli, ['-vs', '-1', 'sample/goog.ods', 'goog.html'])
