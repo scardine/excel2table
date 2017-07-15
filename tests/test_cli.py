f840e2d6491ede9a2733dfba6a4877cb0d5cde70
chfw <wangc_2011@hotmail.com>
2017-07-15 20:39:16 +0100

    more test coverage

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,4 +1,5 @@
 import os
+from mock import patch
 from nose.tools import eq_
 from excel2table.cli import cli
 from click.testing import CliRunner
@@ -10,3 +11,20 @@ def test_cli():
     result = runner.invoke(cli, ['sample/goog.ods', 'goog.html'])
     eq_(result.exit_code, 0)
     os.unlink('goog.html')
+
+
+def test_vs():
+    runner = CliRunner()
+
+    result = runner.invoke(cli, ['-vs', '-1', 'sample/goog.ods', 'goog.html'])
+    eq_(result.exit_code, 0)
+    os.unlink('goog.html')
+
+
+@patch('webbrowser.open')
+@patch('time.sleep', side_effect=KeyboardInterrupt())
+def test_serve(fake_time, fake_webbrowser):
+    runner = CliRunner()
+
+    result = runner.invoke(cli, ['-s', 'sample/goog.ods'])
+    eq_(result.exit_code, 0)
